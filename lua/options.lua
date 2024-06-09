8e5cbdab96f4087704047adad92d9314211af300
Petr Jeníček <petr.jenicek@example.com>
2024-06-09 19:01:15 +0200

    init commit

---
+++ lua/options.lua
@@ -0,0 +1,44 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = false
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+
+vim.opt.timeoutlen = 300
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
---
+++ lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+    "ThePrimeagen/vim-be-good"
+}
+
